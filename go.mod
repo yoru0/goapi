a280104562af4c0fd1137319e8d258962f5526ad
yoru <jordanchandra1@gmail.com>
2025-09-12 00:34:38 +0700

    .

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/yoru0/goapi.git
 
 go 1.24.3
 
-require github.com/go-chi/chi/v5 v5.2.3 // indirect
+require (
+	github.com/go-chi/chi/v5 v5.2.3
+	github.com/google/uuid v1.6.0
+)
--- internal/pkg/models/user.go
+++ internal/pkg/models/user.go
@@ -3,9 +3,9 @@ package models
 import "time"
 
 type User struct {
-	ID          string    `json:"id"`
-	Name        string    `json:"name"`
-	Email       string    `json:"email"`
-	CreatedTime time.Time `json:"createdTime"`
-	UpdatedTime time.Time `json:"updatedTime"`
+	ID        string    `json:"id"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
 }
